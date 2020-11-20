e5e6db431cdfbc8922b74d4f35284e6af1bd0eef
Martian Lapadatescu <arc@ATM0561225DYN.dhcp.missouri.edu>
2020-11-19 19:46:46 -0600

    "the timer component is working"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimerComponent } from './components/timer/timer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/timer/timer.component.ts
@@ -0,0 +1,94 @@
+import {  OnInit } from '@angular/core';
+import { Component, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { Subject, interval } from 'rxjs';
+import { NgModule } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+export interface Entry {
+  created: Date;
+  id: string;
+}
+
+export interface TimeSpan {
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.css']
+})
+export class TimerComponent implements OnInit {
+  constructor(private changeDetector: ChangeDetectorRef) {
+
+  }
+
+  entries: Entry[] = [
+    { id: 'yesterday', created: new Date(new Date().getTime() - (24 * 1000 * 60 * 60) ) },
+    { id: '1 hour ago', created: new Date(new Date().getTime() - (1000 * 60 * 60) ) },
+    { id: '1 minute ago', created: new Date(new Date().getTime() - (1000 * 60) ) }
+  ];
+  newId: string;
+  
+  private destroyed$ = new Subject();
+
+  ngOnInit() {
+    this.newId = 'first';
+    this.addEntry();
+    
+    interval(1000).subscribe(() => {
+      if (!this.changeDetector['destroyed']) {
+        this.changeDetector.detectChanges();
+      }
+    });
+
+    this.changeDetector.detectChanges();
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
+  
+  addEntry() {
+    this.entries.push({
+      created: new Date(),
+      id: this.newId
+    });
+    this.newId = '';
+  }
+
+  getElapsedTime(entry: Entry): TimeSpan {        
+    let totalSeconds = Math.floor((new Date().getTime() - entry.created.getTime()) / 1000);
+    
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+
+    if (totalSeconds >= 3600) {
+      hours = Math.floor(totalSeconds / 3600);      
+      totalSeconds -= 3600 * hours;      
+    }
+
+    if (totalSeconds >= 60) {
+      minutes = Math.floor(totalSeconds / 60);
+      totalSeconds -= 60 * minutes;
+    }
+
+    seconds = totalSeconds;
+    
+    return {
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+    };
+  }
+}
+
+  
+
+
