c99401988c9661fa9d514f3d95afb9a12e9383cd
Austin Ibeh <mailaustinibeh@gmail.com>
2020-11-26 00:00:40 -0600

    login ready

--- Hack-dev/src/app/app-routing.module.ts
+++ Hack-dev/src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { AboutComponent } from './components/about/about.component';
+import { LoginComponent } from './components/login/login.component'
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {CanDeactivateGuard} from './services/can-deactivate-guard.service'
-const routes: Routes = [{path:'', redirectTo:'/home',pathMatch:'full'},
+import {CanDeactivateGuard} from './services/can-deactivate-guard.service';
+import {
+  AngularFireAuthGuard,
+  redirectUnauthorizedTo,
+  redirectLoggedInTo,
+} from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToDashboard = () => redirectLoggedInTo(['']);
+
+const routes: Routes = [
+
+{ path:'', redirectTo:'/home',pathMatch:'full' },
 {path:'home',component:HomeComponent,canDeactivate:[CanDeactivateGuard]},
-{path:'about',component:AboutComponent}
+{path:'about',component:AboutComponent},
+{ path: 'login',component: LoginComponent, canActivate: [AngularFireAuthGuard],data: { authGuardPipe: redirectLoggedInToDashboard },},
+
 
 ];
 
--- Hack-dev/src/app/app.module.ts
+++ Hack-dev/src/app/app.module.ts
@@ -8,13 +8,49 @@ import { AppComponent } from './app.component';
 import { TimerComponent } from './components/timer/timer.component';
 import { HomeComponent } from './components/home/home.component';
 import {TimeService} from './services/time.service';
-import { AboutComponent } from './components/about/about.component'
+import { AboutComponent } from './components/about/about.component';
+import { MatSliderModule } from '@angular/material/slider';
+
 import {CanDeactivateGuard} from './services/can-deactivate-guard.service';
+import { AngularFirestore, AngularFirestoreModule } from "@angular/fire/firestore";
+
+import { LoginComponent } from './components/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { Custom404Component } from './pages/custom404/custom404.component';
+// import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// import { AddProfessorComponent } from './components/add-professor/add-professor.component';
+// import { NavbarComponent } from './components/navbar/navbar.component';
+// import { ClassDetailsComponent } from './pages/class-details/class-details.component';
+
 import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+// import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { HttpClientModule } from '@angular/common/http';
+
+// import { ChartsModule } from 'ng2-charts';
+// import { DetailsComponent } from './pages/details/details.component';
+
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
     TimerComponent,
     HomeComponent,
     AboutComponent,
@@ -23,9 +59,28 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
+    BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    AngularFireAuthModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    // FlexLayoutModule,
+    HttpClientModule,
+    // ChartsModule,
   ],
   //providers: [TimeService ],
   providers:[CanDeactivateGuard,TimeService],
